272d0621204a0aac4e4abc9cf8f950b6b1f34d58
vokoshyv <ronsuij@gmail.com>
2016-01-08 16:22:15 -0800

    (feat) completes tests for sorting_1.rb and sorting_1.js

--- homework_folder/week1day3/sorting_1.rb
+++ homework_folder/week1day3/sorting_1.rb
@@ -40,3 +40,112 @@
  #   **********************************************************/
 
 
+def insertionSort(input)
+  return input
+end
+
+
+def selectionSort(input)
+  return input
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# //////////////////////////////////////////////////////////
+# ///////////////  DO NOT TOUCH TEST BELOW!!!  /////////////
+# //////////////////////////////////////////////////////////
+
+require 'test/unit'
+
+class InsertionSortTest < Test::Unit::TestCase
+  def test_insertionSort_should_handle_example_case
+    test = insertionSort([3,9,1,4,7])
+    answer = [1,3,4,7,9]
+
+    assert_equal(answer, test);
+  end
+
+  def test_insertionSort_should_handle_empty_input
+    test = insertionSort([])
+    answer = []
+
+    assert_equal(answer, test);
+  end
+
+  def test_insertionSort_should_handle_large_input
+    testInput = []
+    $i = 1000000
+
+    while $i > 0
+      toPush = Random.rand(1000000)
+      testInput.push(toPush)
+      $i = $i - 1
+    end
+    
+    test = insertionSort(testInput)
+    assert_equal(testInput.sort, test)
+  end
+
+end
+
+class SelectionSortTest < Test::Unit::TestCase
+  def test_selectionSort_should_handle_example_case
+    test = selectionSort([8,3,2,10])
+    answer = [2,3,8,10]
+
+    assert_equal(answer, test);
+  end
+
+  def test_selectionSort_should_handle_empty_input
+    test = selectionSort([])
+    answer = []
+
+    assert_equal(answer, test);
+  end
+
+  def test_selectionSort_should_handle_large_input
+    testInput = []
+    $i = 1000000
+
+    while $i > 0
+      toPush = Random.rand(1000000)
+      testInput.push(toPush)
+      $i = $i - 1
+    end
+    
+    test = selectionSort(testInput)
+    assert_equal(testInput.sort, test)
+  end
+
+end
