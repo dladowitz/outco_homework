5bf3fae623b16d6ce883ff4d1c3d009b77ecd7f2
vokoshyv <ronsuij@gmail.com>
2016-01-11 22:12:07 -0800

    (feat) modularizes adding each homework assignment's tasks

--- gulpfile.js
+++ gulpfile.js
@@ -19,35 +19,10 @@ var addData = function(date, title){
   ]));
 }
 
-var paths = {
-  week1day2JS: 'homework_folder/week1day2/recursion_1.js', 
-  week1day2RB: 'homework_folder/week1day2/recursion_1.rb', 
-  week1day3JS: 'homework_folder/week1day3/sorting_1.js', 
-  week1day3RB: 'homework_folder/week1day3/sorting_1.rb'
-}
+var paths = {}
 
-// week1day2
-gulp.task('week1day2JS', shell.task([
-  'clear', 
-  'mocha ' + paths['week1day2JS']
-]));
-
-gulp.task('week1day2RB', shell.task([
-  'clear', 
-  'ruby -I"lib:test" ' + paths['week1day2RB']
-]));
-
-// week1day3
-gulp.task('week1day3JS', shell.task([
-  'clear', 
-  'mocha ' + paths['week1day3JS']
-]));
-
-gulp.task('week1day3RB', shell.task([
-  'clear', 
-  'ruby -I"lib:test" ' + paths['week1day3RB']
-]));
-
+addData('week1day2', 'recursion_1'); // adds week1day2
+addData('week1day3', 'sorting_1'); // adds week1day3
 addData('week2day1', 'dataStructures_1'); // adds week2day1
 
 
