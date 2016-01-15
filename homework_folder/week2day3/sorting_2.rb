d0c79643dbb20bbdc0e9543acdf3d497057100f0
Ron Tsui <ronsuij@gmail.com>
2016-01-15 13:52:14 -0800

    (feat) initializes sorting_2.js and sorting_2.rb files

---
+++ homework_folder/week2day3/sorting_2.rb
@@ -0,0 +1,40 @@
+ # /********************************************************************** 
+ #  *                          Homework VI                               *
+ #  *                                                                    *
+ #  *  Problem: Quicksort                                                *
+ #  *                                                                    *
+ #  *  Prompt: Given an unsorted array of numbers,                       *
+ #  *          return a sorted array using Quicksort sort.               *
+ #  *                                                                    *
+ #  *  Input:  An unsorted array                                         *
+ #  *  Output: A sorted array                                            *
+ #  *                                                                    *
+ #  *  Example: input = [3,9,1,4,7] , output = [1,3,4,7,9]               *
+ #  *                                                                    *
+ #  *  What are the time and auxilliary space complexity?                *
+ #  *                                                                    *
+ #  **********************************************************************/
+
+
+
+
+
+
+
+ #  /**********************************************************
+ #   *             Highly Recommended Extra Credit            *
+ #   *                                                        *
+ #   *  Problem: Bubble Sort                                  *
+ #   *                                                        *
+ #   *  Prompt: Given an unsorted array of numbers,           *
+ #   *          return a sorted array using bubble sort.      *
+ #   *                                                        *
+ #   *  Input: An unsorted array                              *
+ #   *  Output: A sorted array                                *
+ #   *                                                        *
+ #   *  Example: input = [8,3,2,10] output = [2,3,8,10]       *
+ #   *                                                        *
+ #   *  What are the time and auxilliary space complexity?    *
+ #   *                                                        *
+ #   **********************************************************/
+
