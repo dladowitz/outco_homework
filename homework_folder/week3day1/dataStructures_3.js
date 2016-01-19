c92198799ad86f11803880128c3454ad5d27a67e
Ron Tsui <ronsuij@gmail.com>
2016-01-18 21:32:36 -0800

    (feat) adds tests for dataStructures_3.js

--- homework_folder/week3day1/dataStructures_3.js
+++ homework_folder/week3day1/dataStructures_3.js
@@ -139,108 +139,176 @@ describe('hash table class ', function(){
       expect(test).to.have.property('storage');
       expect(test).to.have.property('buckets');
       expect(test).to.have.property('size');
-      expect(test.root).to.equal(null);
+      expect(test.storage.length).to.equal(0);
       expect(test.size).to.equal(0);
     });
   });
 
   describe('hashTable methods existence', function(){
-    it('should have methods insert, search, and delete', function(){
+    it('should have methods hash, insert, delete, and retrieve', function(){
       var test = new hashTable();
-
+      
+      expect(test).to.respondTo('hash');
       expect(test).to.respondTo('insert');
-      expect(test).to.respondTo('search');
       expect(test).to.respondTo('delete');
+      expect(test).to.respondTo('retrieve');
     });
   });
 
-  describe('hashTable insert method', function(){
-    it('should be able to insert a single node', function(){
+  describe('hashTable hash method', function(){
+    it('should return an index from an inputted string', function(){
       var test = new hashTable();
 
-      expect(test.root).to.equal(null);
+      var expected = test.hash('hello');
+      expect(expected).to.equal(1);
+    })
+  });
+
+  describe('hashTable insert method', function(){
+    it('should be able to insert a key-value pair', function(){
+      var test = new hashTable();
+
+      expect(test.storage.length).to.equal(0);
       expect(test.size).to.equal(0);
 
-      test.insert(5);
+      test.insert('hello', 5);
 
-      expect(test.root.value).to.equal(5);
-      expect(test.root.rightChild).to.equal(null);
-      expect(test.root.leftChild).to.equal(null);
       expect(test.size).to.equal(1);
+      expect(test.storage[1][0][0]).to.equal('hello');
+      expect(test.storage[1][0][1]).to.equal(5);
     });
 
-    it('should be able to insert a second node', function(){
+    it('should be able to insert a second key-value pair', function(){
       var test = new hashTable();
 
-      expect(test.root).to.equal(null);
+      expect(test.storage.length).to.equal(0);
       expect(test.size).to.equal(0);
 
-      test.insert(5);
+      test.insert('hello', 5);
 
-      expect(test.root.value).to.equal(5);
-      expect(test.root.rightChild).to.equal(null);
-      expect(test.root.leftChild).to.equal(null);
       expect(test.size).to.equal(1);
+      expect(test.storage[1][0][0]).to.equal('hello');
+      expect(test.storage[1][0][1]).to.equal(5);
 
-      test.insert(10);
+      test.insert('good', 10);
 
-      expect(test.root.value).to.equal(5);
-      expect(test.root.rightChild.value).to.equal(10);
-      expect(test.root.leftChild).to.equal(null);
+      expect(test.size).to.equal(2);
+      expect(test.storage[6][0][0]).to.equal('good');
+      expect(test.storage[6][0][1]).to.equal(10);
+    });
+
+    it('should be able to handle collisions', function(){
+      var test = new hashTable();
+
+      expect(test.storage.length).to.equal(0);
+      expect(test.size).to.equal(0);
+
+      test.insert('good', 5);
+
+      expect(test.size).to.equal(1);
+      expect(test.storage[6][0][0]).to.equal('good');
+      expect(test.storage[6][0][1]).to.equal(5);
+
+      test.insert('back', 10);
+
+      expect(test.size).to.equal(2);
+      expect(test.storage[6][1][0]).to.equal('back');
+      expect(test.storage[6][1][1]).to.equal(10);
+    });
+  });
+
+  describe('hashTable delete method', function(){
+    it('should delete a key-value pair', function(){
+      var test = new hashTable();
+
+      expect(test.storage.length).to.equal(0);
+      expect(test.size).to.equal(0);
+
+      test.insert('hello', 5);
+
+      expect(test.size).to.equal(1);
+      expect(test.storage[1][0][0]).to.equal('hello');
+      expect(test.storage[1][0][1]).to.equal(5);
+
+      test.delete('hello');
+
+      expect(test.size).to.equal(0);
+      expect(test.storage[1][0][0]).to.equal(undefined);
+      expect(test.storage[1][0][1]).to.equal(undefined);
+
+    });
+
+    it('should not modify the size when deleting a key-value pair that does not exist', function(){
+      var test = new hashTable();
+      test.insert('hello', 5);
+      test.insert('good', 10);
+
+      expect(test.size).to.equal(2);
+      
+      test.delete('great');
+      
       expect(test.size).to.equal(2);
     });
   });
 
-  describe('hashTable search method', function(){
-    it('should return true when the node exists', function(){
+  describe('hashTable retrieve method', function(){
+    it('should return true for a key-value pair that exists', function(){
       var test = new hashTable();
-      test.insert(5);
-      test.insert(10);
 
-      expect(test.search(5)).to.equal(true);
-    })
+      test.insert('hello', 5);
 
-    it('should return false when the node does not exist', function(){
-      var test = new hashTable();
-      test.insert(5);
-      test.insert(10);
+      expect(test.retrieve('hello')).to.equal(5);
 
-      expect(test.search(17)).to.equal(false);
-    })
-  })
-
-  describe('hashTable delete method', function(){
-    it('should delete a node', function(){
-      var test = new hashTable();
-      test.insert(5);
-      test.insert(10);
-      test.insert(2);
-      test.insert(7);
-      test.insert(1);
-
-      expect(test.search(7)).to.equal(true);
-      
-      test.delete(7);
-      
-      expect(test.search(7)).to.equal(false);
 
     });
 
-    it('should modify the size when deleting a node', function(){
+    it('should return false for a key-value pair that does not exist', function(){
       var test = new hashTable();
-      test.insert(5);
-      test.insert(10);
-      test.insert(2);
-      test.insert(7);
-      test.insert(1);
 
-      expect(test.size).to.equal(5);
+      test.insert('hello', 5);
       
-      test.delete(7);
-      
-      expect(test.size).to.equal(4);
+      expect(test.retrieve('good')).to.equal(null);
+    });
+  });
+
+  describe('hashTable resize method', function(){
+    it('should double the number of buckets when the size exceeds 75% of buckets capacity', function(){
+      var test = new hashTable();
+
+      test.insert('hello', 5);
+      test.insert('good', 7);
+      test.insert('haha', 10);
+      test.insert('blah', 2);
+      test.insert('foo', 3);
+      test.insert('bar', 8);
+      test.insert('taste', 1);
+
+      expect(test.buckets).to.equal(16);
+
+
     });
 
+    it('should halve the number of buckets when the size drops below 25% of bucket capacity', function(){
+      var test = new hashTable();
+
+      test.insert('hello', 5);
+      test.insert('good', 7);
+      test.insert('haha', 10);
+      test.insert('blah', 2);
+      test.insert('foo', 3);
+      test.insert('bar', 8);
+      test.insert('taste', 1);
+
+      expect(test.buckets).to.equal(16);
+
+      test.delete('hello');
+      test.delete('good');
+      test.delete('haha');
+      test.delete('blah');
+
+      expect(test.buckets).to.equal(8);
+
+    });
   });
 
 })
