ee3b3ad7b1d0ae396b4b2ed3b326cac836ae4968
Ron Tsui <rontsui@Rons-MacBook-Pro.local>
2016-01-22 20:26:02 -0800

    (feat) initializes files for dynamicProg_1.js and dynamicProg_1.rb

---
+++ homework_folder/week3day3/dynamicProg_1.rb
@@ -0,0 +1,47 @@
+ # /********************************************************************** 
+ #  *                           Homework X                               *
+ #  **********************************************************************/
+
+ # /********************************************************************** 
+ #  *  Problem 1: Max Consecutive Sum                                    *
+ #  *                                                                    *
+ #  *  Prompt: Given an array of integers find the sum of consecutive    *
+ #  *          values in the array that produces the maximum value.      *
+ #  *                                                                    *
+ #  *  Input:  Unsorted array of positive and negative integers          *
+ #  *  Output: Integer (max consecutive sum)                             *
+ #  *                                                                    *
+ #  *  Time Complexity: O(n)                                             *
+ #  *  Auxiliary Space Complexity: O(1)                                  *
+ #  *                                                                    *
+ #  *  Example: input = [6, -1, 3, 5, -10]                               *
+ #  *           output = 13 (6 + -1 + 3 + 5 = 13)                        *
+ #  *                                                                    *
+ #  **********************************************************************/
+
+ # /**********************************************************************
+ #  *  Problem 2: Coin Change                                            *
+ #  *                                                                    *
+ #  *  Prompt: US Currency has coins in circulation with the             *
+ #  *          following value in cents: [1, 5, 10, 25, 50, 100]         *
+ #  *                                                                    *
+ #  *          Given a value, find how many unique ways to make change   *
+ #  *          given the coin values in circulation.                     *
+ #  *                                                                    *
+ #  *  Input:  An integer N (value from which to make change)            *
+ #  *  Output: An integer (number of unique ways to make change)         *
+ #  *                                                                    *
+ #  *  Time Complexity: O(nm)                                            *
+ #  *  Auxiliary Space Complexity: O(n)                                  *
+ #  *                                                                    *
+ #  *  Example: input = 11                                               *
+ #  *           output = 4 ([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],           *
+ #  *                       [5, 1, 1, 1, 1, 1, 1],                       *
+ #  *                       [5, 5, 1],                                   *
+ #  *                       [10, 1])                                     *
+ #  *                                                                    *
+ #  *  Hint: www.geeksforgeeks.org/dynamic-programming-set-7-coin-change *
+ #  *                                                                    *
+ #  **********************************************************************/
+
+
