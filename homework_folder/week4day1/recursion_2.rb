0a27f414b42ba70ef680f9fd35ff4df0920c2c90
Ron Tsui <ronsuij@gmail.com>
2016-01-25 14:21:19 -0800

    (feat) adds tests for recursion_2.rb

--- homework_folder/week4day1/recursion_2.rb
+++ homework_folder/week4day1/recursion_2.rb
@@ -26,3 +26,72 @@
  #  *************************************************************************/
 
 
+def latticePaths(n)
+  
+end
+
+
+
+
+
+
+
+
+
+
+# //////////////////////////////////////////////////////////
+# ///////////////  DO NOT TOUCH TEST BELOW!!!  /////////////
+# //////////////////////////////////////////////////////////
+
+require 'test/unit'
+
+class LatticePathsTest < Test::Unit::TestCase
+
+  def test_small_lattice_2
+    puts ': input 2 should expect 6'
+    test = latticePaths(2)
+    answer = 6
+
+    assert_equal(answer, test)
+  end
+
+  def test_small_lattice_1
+    puts ': input 1 should expect 2'
+    test = latticePaths(1)
+    answer = 2
+
+    assert_equal(answer, test)
+  end
+
+  def test_small_lattice_3
+    puts ': input 3 should expect 20'
+    test = latticePaths(3)
+    answer = 20
+
+    assert_equal(answer, test)
+  end
+
+  def test_large_lattice_20
+    puts ': input 20 should expect 137846528820'
+    test = latticePaths(20)
+    answer = 137846528820
+
+    assert_equal(answer, test)
+  end
+
+  def test_edge_case_lattice_0
+    puts ': input 0 should expect 1'
+    test = latticePaths(0)
+    answer = 1
+
+    assert_equal(answer, test)
+  end  
+
+end
+
+
+
+
+
+
+
