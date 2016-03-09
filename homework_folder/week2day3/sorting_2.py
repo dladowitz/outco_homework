96b60991938a3c5a44281364eed13a14b669f27c
vokoshyv <ronsuij@gmail.com>
2016-03-08 20:53:35 -0800

    (feat) adds python problem sorting_2.py

---
+++ homework_folder/week2day3/sorting_2.py
@@ -0,0 +1,45 @@
+ # /********************************************************************** 
+ #  *                          Homework VI                               *
+ #  *                                                                    *
+ #  *  Problem: Quicksort                                                *
+ #  *                                                                    *
+ #  *  Prompt: Given an unsorted array of numbers,                       *
+ #  *          return a sorted array using Quicksort sort.               *
+ #  *                                                                    *
+ #  *  Input:  An unsorted array                                         *
+ #  *  Output: A sorted array                                            *
+ #  *                                                                    *
+ #  *  Example: input = [3,9,1,4,7] , output = [1,3,4,7,9]               *
+ #  *                                                                    *
+ #  *  What are the time and auxilliary space complexity?                *
+ #  *                                                                    *
+ #  **********************************************************************/
+
+ # /********************************************************************** 
+ #  *                                                                    *
+ #  *  Problem: Mergesort                                                *
+ #  *                                                                    *
+ #  *  Prompt: Given an unsorted array of numbers,                       *
+ #  *          return a sorted array using Mergesort sort.               *
+ #  *                                                                    *
+ #  *  Input:  An unsorted array                                         *
+ #  *  Output: A sorted array                                            *
+ #  *                                                                    *
+ #  *  Example: input = [3,9,1,4,7] , output = [1,3,4,7,9]               *
+ #  *                                                                    *
+ #  *  What are the time and auxilliary space complexity?                *
+ #  *                                                                    *
+ #  **********************************************************************/
+
+
+def quickSort(input): 
+  # your work here
+
+
+def mergeSort(input):
+  # your work here
+
+
+
+
+
