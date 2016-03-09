2a6140add83f3e976548099aa4f839446dcfe85b
vokoshyv <ronsuij@gmail.com>
2016-03-08 20:01:53 -0800

    (feat) adds python problem recursion_1.py

---
+++ homework_folder/week1day2/recursion_1.py
@@ -0,0 +1,20 @@
+# /********************************************************************** 
+#  *                          Homework II                               *
+#  *                                                                    *
+#  *  Prompt: Given a set S, return the power set P(S), which is        *
+#  *          a set of all subsets of S.                                *
+#  *                                                                    *
+#  *  Input:  A String                                                  *
+#  *  Output: An Array with the power set of the input string           *
+#  *                                                                    *
+#  *  Example: S = "abc", P(S) = ['', 'a', 'b','c','ab','ac','bc','abc']*                                                               *
+#  *                                                                    *
+#  *  Note: There should not be any duplicates in the power set         *
+#  *        ('ab' and 'ba' are considered the same), and it will always *
+#  *        begin with an empty string ('')                             *
+#  *                                                                    *
+#  **********************************************************************/
+
+def powerSet(input): 
+  # your work here
+  
